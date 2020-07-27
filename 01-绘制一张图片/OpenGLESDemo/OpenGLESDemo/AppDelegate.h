63e7cda5328b72a2950f4838b949c10dca4dd8e1
zhiqingchun <470030689@qq.com>
2020-07-27 16:30:52 +0800

    Add files via upload

---
+++ 01-绘制一张图片/OpenGLESDemo/OpenGLESDemo/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  OpenGLESDemo
+//
+//  Created by lijiang on 2020/7/27.
+//  Copyright © 2020 lijiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
